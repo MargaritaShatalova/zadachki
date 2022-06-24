be8c1c5fd1f95fd6b467fbaaf35ac01087a5cc70
MargaritaShatalova <106661458+MargaritaShatalova@users.noreply.github.com>
2022-06-24 14:14:59 +0300

    2

--- заменить слово в строке.py
+++
@@ -1,7 +0,0 @@
-# заменить слова в строке на другие слова
-example_string = "У нас сегодня Иван"
-example_string = example_string.replace("Иван", "Михаил")
-print(example_string)
-
-# проще вариант
-print('У нас сегодня Иван'.replace('Иван', 'Николай'))
